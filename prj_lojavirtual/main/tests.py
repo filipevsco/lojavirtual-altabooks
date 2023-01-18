ba851160b361ad1ffe5550837c9ac38e8acaa063
filipevsco <filipev@me.com>
2023-01-17 21:03:33 -0300

    tests main

--- prj_lojavirtual/main/tests.py
+++ prj_lojavirtual/main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestarPaginas(TestCase):
+    def testar_se_pagina_principal_carrega_completamente(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertContains(response, 'Loja Virtual')
