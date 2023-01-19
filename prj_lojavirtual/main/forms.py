ccfe83d8d9e1615a60018e00efba8318508910b0
filipevsco <filipev@me.com>
2023-01-19 17:28:19 -0300

    formulario

--- prj_lojavirtual/main/forms.py
+++ prj_lojavirtual/main/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class FormFaleConosco(forms.Form):
-    nome = forms.CharField(required=True)
+    nome = forms.CharField(required=True, initial="Digite seu nome")
     email_origem = forms.EmailField(required=True, label= 'Entre com seu e-mail:')
     mensagem = forms.CharField(required=True)
--- prj_lojavirtual/main/views.py
+++ prj_lojavirtual/main/views.py
@@ -6,3 +6,7 @@ class ViewFaleConosco(FormView):
     template_name = "fale_conosco.html"
     form_class = forms.FormFaleConosco
     success_url = "/"
+
+    def form_valid(self, form):
+        form.enviar_mensagem_por_email()
+        return super().form_valid(form)
