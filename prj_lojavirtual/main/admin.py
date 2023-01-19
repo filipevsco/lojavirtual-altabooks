60bdc049438887ac588b57d46a50da5ac9187472
filipevsco <filipev@me.com>
2023-01-19 19:06:15 -0300

    admin ok

--- prj_lojavirtual/main/admin.py
+++ prj_lojavirtual/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'slug']
+    prepopulated_fields = {'slug': ('nome',)}
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'slug', 'preco', 'estoque', 'disponivel', 'data_criacao', 'data_ultima_atualizacao']
+    list_editable = ['preco', 'estoque', 'disponivel']
+    prepopulated_fields = {'slug': ('nome',)}
