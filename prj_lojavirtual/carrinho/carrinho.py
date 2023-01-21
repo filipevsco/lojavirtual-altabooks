ebfeb3cfade25b99406b991e57a6753cbec393ce
filipevsco <filipev@me.com>
2023-01-21 11:50:26 -0300

    implementacao do carrinho

--- prj_lojavirtual/carrinho/carrinho.py
+++ prj_lojavirtual/carrinho/carrinho.py
@@ -13,4 +13,50 @@ class Carrinho:
     def adicionar(self, produto, quantidade=1, atualizar_quantidade=False):
         id_produto = str(produto.id)
         if id_produto not in self.__carrinho:
-        pass
+            self.__carrinho[id_produto] = {
+                'quantidade': 0,
+                'preco': str(produto.preco),
+            }
+        if atualizar_quantidade:
+            self.__carrinho['id_produto']['quantidade'] = quantidade
+        else:
+            self.__carrinho['id_produto']['quantidade'] += quantidade
+        self.__salvar()
+    
+    def __salvar(self):
+        self.__sessao[settings.ID_CARRINHO] = self.__carrinho
+        self.__sessao.modified = True
+
+    def remover(self, produto):
+        id_produto = str(produto.id)
+        if produto.id in self.__carrinho:
+            del self.__carrinho[id_produto]
+            self.__salvar()
+
+    def __iter__(self):
+        ids_produtos = self.__carrinho.keys()
+        produtos = Produto.objects.filter(id__in=ids_produtos)
+        carrinho = self.__carrinho.copy()
+        for produto in produtos:
+            carrinho[str(produto.id)]['produto']=produto
+        for item in carrinho.values():
+            item['preco']=Decimal(item['preco'])
+            item['subtotal']=Decimal(item['preco'])*Decimal(item['quantidade'])
+            yield item
+        
+        def __len__(self):
+            resultado = 0
+            for item in self.__carrinho.values():
+                resultado += item['quantidade']
+            return resultado
+        
+        def get_total_geral(self):
+            resultado = Decimal(0.0)
+            for item in self.__carrinho.values():
+                subtotal = Decimal(item['quantidade']) * Decimal(item['preco'])
+                resultado = resultado + subtotal
+            return resultado
+        
+        def limpar_carrinho(self):
+            del self.__sessao[settings.ID_CARRINHO]
+            self.__salvar()
