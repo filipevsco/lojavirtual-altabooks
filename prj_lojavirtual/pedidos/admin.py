fdc71044f5ca4a43d79d11720e6dc4b11242c7c8
filipevsco <filipev@me.com>
2023-01-25 09:37:37 -0300

    admin pedido criado

--- prj_lojavirtual/pedidos/admin.py
+++ prj_lojavirtual/pedidos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Pedido, ItemPedido
 
-# Register your models here.
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    raw_id_fields = ['pedido']
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'email', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'uf', 'cep', 'data_criacao', 'pago']
+    list_filter = ['pago', 'data_criacao', 'nome']
+    inlines = [ItemPedidoInline]
+
+admin.site.register(Pedido, PedidoAdmin)
